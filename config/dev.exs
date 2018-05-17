e23ee5e8c922e9198780746e16d0f37f713a9a21
Fabian Carlos <fabian.pow@gmail.com>
2018-05-17 14:54:57 -0400

    update db config

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :phoenix_app, PhoenixApp.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "fabiancarlos",
+  password: "",
   database: "phoenix_app_dev",
   hostname: "localhost",
   pool_size: 10
